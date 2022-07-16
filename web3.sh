6237dcea55b51efaa9d8ed0a9471a7bcbdeb6ce9
Mark Gregory <109108911+Cryptolabgeeks@users.noreply.github.com>
2022-07-16 12:55:33 +0530

    Update web3.sh

--- web3.sh
+++ web3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for ((i=1; i<=10000000; i+=1)) ; do
-  p1=$(shuf -i 204963823041217241-409927646082434481 -n1)
+  p1=$(shuf -i 199849554433877106-679297273601426731 -n1)
   page=$p1
   webserv="https://privatekeys.pw/keys/bitcoin/$page"
 
